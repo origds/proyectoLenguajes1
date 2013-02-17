c74bb4cb88d3e32bc3e0ea408bb0ff828101c069
Oriana Gomez <origds@gmail.com>
2013-02-17 10:56:21 -0430

    areglando errores

--- Batalla.hs
+++ Batalla.hs
@@ -12,11 +12,31 @@ atacar n mons
   | otherwise = Just $ fst atack
     where
       atack = (listarAtaque (ataques mons)) !! n 
-      
+ 
 -- Funcion para cambiar de monstruo en la batalla
 
-cambiar :: Int -> [Monstruo] -> Monstruo
-cambiar n lista = lista !! n
+cambiar :: Int -> [Monstruo] -> Maybe Monstruo
+cambiar n lista
+  | hpactual (lista !! n) == 0 = Nothing
+  | otherwise = Just (lista !! n)
+  
+-- Funcion para obtener informacion de un monstruo 
+
+info :: String -> [Monstruo] -> Monstruo
+info s listaEntrenador = fromJust $ elMonstruo s
+  where
+    elMonstruo :: String -> Maybe Monstruo
+    elMonstruo s = obtenerMonstruo s (mapaMonstruo listaMonstruo)
+      where
+        mapaMonstruo :: [Monstruo] -> M.Map String Monstruo
+        mapaMonstruo lista = foldl' combinar M.empty lista
+          where
+            combinar mapaViejo monstruoNuevo = M.insert (sobrenombre monstruoNuevo) ataqueNuevo mapaViejo
+    
+        obtenerMonstruo :: String -> M.Map String Monstruo -> Maybe Monstruo
+        obtenerMonstruo nombre mapa = M.lookup nombre mapa
+ 
+
 
 
 
